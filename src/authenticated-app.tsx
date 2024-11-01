a9ddf47c90172d9152bf44d0a1b7cf9f4102e659
mr8eight <3034442031@qq.com>
2024-11-01 15:27:14 +0800

    style:用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,43 @@
 import { ProjectListScreen } from "screens/project-list"
 import { useAuth } from "context/auth-context"
+import { stderr } from "process";
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthenticatedApp = () => {
     const {logout} =useAuth()
-    return <div>
-        <button onClick={logout}>登出</button>
-        <ProjectListScreen/>
-    </div>
-}
+    return (
+    <Container>
+        <Header between={true}> 
+        <HeaderLeft gap={true}>
+            <h2>logo</h2>
+            <h2>项目</h2>
+            <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+            <button onClick={logout}>登出</button>
+        </HeaderRight>
+        </Header>
+        <Main>
+            <ProjectListScreen/>
+        </Main>
+    </Container>
+    );
+};
 
-export {};
+const Container = styled.div`
+    display: grid;
+    grid-template-rows:6rem 1fr;
+    height: 100vh;
+`;
+const Header = styled(Row)`
+
+`;
+
+const HeaderLeft = styled(Row)`
+
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+
+`;
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -48,4 +48,4 @@ export const useHttp = ()=> {
     const {user} = useAuth()
     return (...[endpoint,config]:Parameters<typeof http>) => 
         http(endpoint,{...config,token: user?.token})
-}
+}   
