5aff3055a2b52112aae48294841b35330d7159d4
mr8eight <3034442031@qq.com>
2024-11-02 20:53:40 +0800

    feat:处理me接口 loading和error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,12 +1,27 @@
 import React, { Provider, ReactNode, useState } from "react";
 import * as auth from 'auth-provider'
 import {User} from 'screens/project-list/search-panel'
+import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
+import { http } from "utils/http";
 
 
 interface AuthForm{
     username:string,
     password:string
 }
+
+const bootstrapUser = async() => {
+    let user = null
+    const token = auth.getToken()
+    if(token){
+        const data = await http('me',{token})
+        user = data.user
+    } 
+    return user
+}
+
 const AuthContext = React.createContext<{
     user:User | null,
     register:(form:AuthForm) => Promise<void>,
@@ -17,12 +32,23 @@ const AuthContext = React.createContext<{
 AuthContext.displayName = 'AuthContext'
      
 export const AuthProvider = ({children}:{children:ReactNode}) => {
-    const [user,setUser] = useState<User | null>(null)
+    const {data:user,error,isLoading,isIdle,isError,run,setData:setUser} = useAsync<User | null>()
 
     const login = (form:AuthForm) => auth.login(form).then(setUser)
     const register = (form:AuthForm) => auth.register(form).then(setUser)
     const logout = () => auth.logout().then(()=>setUser(null))
 
+    useMount(()=>{
+        run(bootstrapUser())
+    });
+
+    if(isIdle||isLoading){
+        return <FullPageLoading/>
+    }
+
+    if(isError){
+        return <FullPageErrorFallback error={error} />
+    }
     return <AuthContext.Provider children={children} value={{user,login,register,logout}}/>
 }
 
@@ -35,3 +61,4 @@ export const useAuth = () => {
     }
     return context
 }
+
