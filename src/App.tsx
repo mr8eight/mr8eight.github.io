2a98ae9702947a48aedc672e14d2ff47cb83f1f5
mr8eight <3034442031@qq.com>
2024-11-14 10:41:57 +0800

    feat:代码分割

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { useAuth } from "context/auth-context";
 import { ErrorBoundary } from "components/error-boundary";
 import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 
-const AuthenticatedApp = React.lazy(() => import("authenticated-app"));
-const UnauthenticatedApp = React.lazy(() => import("unauthenticated-app"));
+const AuthenticatedApp = React.lazy(() => import("authenticated-app")); //代码分割优化性能
+const UnauthenticatedApp = React.lazy(() => import("unauthenticated-app")); 
 
 function App() {
   const { user } = useAuth();
 
   return (
     <div className="App">
-      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}> 
+      {/* 代码分割优化性能 */}
         <React.Suspense fallback={<FullPageLoading />}>
           {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
         </React.Suspense>
